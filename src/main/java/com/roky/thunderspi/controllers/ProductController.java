b04335fccf4d838c931176352aad7efc492158da
faroukbs <farouk.boussaid@esprit.tn>
2023-03-20 02:04:15 +0100

    product controller

--- src/main/java/com/roky/thunderspi/controllers/ProductController.java
+++ src/main/java/com/roky/thunderspi/controllers/ProductController.java
@@ -1,7 +1,42 @@
 package com.roky.thunderspi.controllers;
+
+import com.roky.thunderspi.entities.Product;
+import com.roky.thunderspi.services.IProductService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
-public class ProductController{
+@AllArgsConstructor
+public class ProductController {
+    private IProductService iProductService;
+
+
+    @GetMapping("/getAll")
+    public List<Product> findAllProducts() {
+        return iProductService.findAllProducts();
+    }
+
+    @GetMapping("getAll/{id}")
+    public Product findProdById(@PathVariable Long id) {
+        return iProductService.findProdById(id);
+    }
+
+    @PostMapping("/add")
+    public Product addProduct(@RequestBody Product product) {
+        return iProductService.addProduct(product);
+    }
+
+    @PutMapping("update")
+    public Product editProduct(@RequestBody Product product) {
+        return iProductService.addProduct(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        iProductService.deleteProduct(id);
+    }
+
 }
