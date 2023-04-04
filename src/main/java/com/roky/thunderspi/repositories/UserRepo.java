f50a924cb9778721ce7eb8fd9d874ba0b9d0ef91
faroukbs <farouk.boussaid@esprit.tn>
2023-04-04 20:00:09 +0100

    Integration of Shop and Project modules

--- src/main/java/com/roky/thunderspi/repositories/UserRepo.java
+++ src/main/java/com/roky/thunderspi/repositories/UserRepo.java
@@ -2,6 +2,8 @@ package com.roky.thunderspi.repositories;
 
 import com.roky.thunderspi.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<User,Long> {
 }
